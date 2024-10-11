05d8238a99e514fb623d9cd304ff6fc542089487
rhx <wongethan120@gmail.com>
2024-10-11 09:18:45 +0000

    End of Lesson 13(?)

--- script/contacts.js
+++ script/contacts.js
@@ -1,3 +1,18 @@
 let validate = () => {
-	// Fill in the necessary validation here
+	alert("Test");
+	let inputFirstName = document.getElementById("firstname");
+	console.log("Name: " + inputFirstName.value);
+
+	if (inputFirstName.value.length > 0) {
+		inputFirstName.classList = "form-control is-valid";
+		document.getElementById("firstname_e").classList = "text-danger d-none";
+	}
+	else {
+		inputFirstName.classList = "form-control is-invalid"
+		document.getElementById("firstname_e").classList = "text-danger";
+		
+	}
+	if(document.getElementsByClassName("is-invalid"))
 }
+
+
