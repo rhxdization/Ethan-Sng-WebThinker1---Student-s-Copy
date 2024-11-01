dbfd6a28afbea7c09220f1a152152faa5cb8100e
rhx <wongethan120@gmail.com>
2024-11-01 11:00:46 +0000

    end lof kfx;g10---17

--- script/projects.js
+++ script/projects.js
@@ -1,1 +1,23 @@
 // 
+
+    window.onload = () => {
+        let canvas = document.getElementById("canvasBoard");
+        console.log(canvas);
+        let ctx = canvas.getContext("2d");
+        console.log(ctx);
+        ctx.fillStyle = "rgb(0, 128, 255)";
+        ctx.strokeStyle = "rgb(0, 0, 0, 0)";
+        ctx.beginPath();
+        ctx.arc(400,300,10,0,2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+
+    }
+
+    let drawBall = () => {
+
+    }
+
+    let refreshAnimation = () => {
+        
+    }
